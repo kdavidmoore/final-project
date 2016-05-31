382932d959fbcc9793bfc3a1d88321c78842c33f
Keith Moore <keith.m4@gmail.com>
2016-05-31 17:31:14 -0400

    added express app, mysql for node, set up some express routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// create a connection to the final-project database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'test',
+	password: 'testPassword!!1',
+	database: 'final-project'
 });
+
+
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+
+router.post('/register', function(req, res, next) {
+	console.log(req.body);
+	
+	/// mysql connection...activate!	
+	connection.connect();
+
+	connection.query('SELECT * FROM users', function(err, rows, fields) {
+		if (err) throw err;
+		console.log('The first row contains', rows[0].name);
+	});
+
+	connection.end();
+	res.redirect('/');
+});
+
+
+router.post('/login', function(req, res, next) {
+	console.log(req.body);
+	
+	/// mysql connection...activate!	
+	connection.connect();
+
+	connection.query('SELECT * FROM users', function(err, rows, fields) {
+		if (err) throw err;
+		console.log('The first row contains', rows[0].name);
+	});
+
+	connection.end();
+	res.redirect('/');
+});
+
+
+router.post('/cancel', function(req, res, next) {
+	console.log(req.body);
+	
+	/// mysql connection...activate!	
+	connection.connect();
+
+	connection.query('SELECT * FROM users', function(err, rows, fields) {
+		if (err) throw err;
+		console.log('The first row contains', rows[0].name);
+	});
+
+	connection.end();
+	res.redirect('/');
+});
+
 
 module.exports = router;
