ae116a40dd88e847bc1dd788480355a134854ee0
Keith Moore <keith.m4@gmail.com>
2016-06-01 08:58:18 -0400

    existing user can now log in

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,40 +20,67 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/register', function(req, res, next) {
-	console.log(req.body);
 
 	var salt = bcrypt.genSaltSync(10);
 	var token = randtoken.generate(32);
 	var newUser = {
 		username: req.body.username,
 		password: bcrypt.hashSync(req.body.password, salt),
-		email: req.body.email
+		email: req.body.email,
+		token: token
 	}
 
 	connection.query('SELECT * FROM `accounts` WHERE `username` = ?', [newUser.username],
-		function(err, rows, fields) {
-		if (err) throw err;
+		function(err, results, fields) {
+			if (err) throw err;
 
-		if (rows.length > 0){
-			res.json({ failure: 'notUnique' });
-		} else {
-			connection.query('INSERT INTO `accounts` SET ?', newUser, function(err, result) {
-				if (err) throw err;
-				else {
-					res.json({
-						success: 'added',
-						token: token
-					});
-				}
-			});
-		}
-	});
-
+			if (results.length > 0) {
+				res.json({ failure: 'notUnique' });
+			} else {
+				connection.query('INSERT INTO `accounts` SET ?', newUser, function(err, result) {
+					if (err) throw err;
+					else {
+						res.json({
+							success: 'added',
+							token: token
+						});
+					}
+				});
+			}
+		});
 });
 
 
 router.post('/login', function(req, res, next) {
-	console.log(req.body);
+	var token = randtoken.generate(32);
+
+	connection.query('SELECT * FROM `accounts` WHERE `username` = ?', [req.body.username],
+		function(err, results, fields) {
+			if (err) throw err;
+
+			if (results.length < 1) {
+				res.json({ failure: 'noUser'});
+			} else {
+
+				var loginResult = bcrypt.compareSync(req.body.password, results[0].password);
+				
+				if (loginResult) {
+
+					connection.query('UPDATE `accounts` SET `token` = ? WHERE `username` = ?',
+						[token, req.body.username], function(err, results) {
+						if (err) throw err;
+						else {
+							res.json({
+								success: 'match',
+								token: token
+							});
+						}
+					});
+				} else {
+					res.json({ failure: 'noMatch' });
+				}
+			}
+	});
 });
 
 
