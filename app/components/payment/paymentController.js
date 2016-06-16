f388a294f744e96ab7b07c41ebac7b8c95e823d8
Keith Moore <keith.m4@gmail.com>
2016-06-16 17:51:05 -0400

    fixed error on payments page

--- app/components/payment/paymentController.js
+++ app/components/payment/paymentController.js
@@ -2,7 +2,6 @@ app.controller('paymentController', ['$state', '$scope', '$stateParams', 'UserAu
 	function($state, $scope, $stateParams, UserAuthService, GetRequestService) {	
 	UserAuthService.checkToken().then(function(result) {
 		if (result.success == 'validated') {
-			
 			$scope.orderType = $stateParams.type;
 			GetRequestService.getUserId().then(function(data) {
 				GetRequestService.getOrderId(data.id).then(function(data) {
